55850ea81c097e771ada94d1d8d4d846b06332d5
Vaibhavi <vppanchaofficial@gmail.com>
2022-02-02 15:52:05 +0530

    Next COMMIT

---
+++ app/src/main/java/com/canvas/drawing/DrawingView1.kt
@@ -0,0 +1,100 @@
+package com.canvas.drawing
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView1(context: Context, attrs:AttributeSet): View(context,attrs) {
+
+    var paint: Paint? = null
+    var paintText: Paint? = null
+    var paintTriangle: Paint? = null
+    var paint8: Paint? = null
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        paint = Paint()
+        paintText = Paint()
+        paintTriangle = Paint()
+        paint8 = Paint()
+
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, wprev: Int, hprev: Int) {
+        super.onSizeChanged(w, h, wprev, hprev)
+
+    }
+
+
+    /**
+     * This method is called when a stroke is drawn on the canvas
+     * as a part of the painting.
+     */
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        //paint?.color = Color.MAGENTA
+        paint!!.isAntiAlias = true
+        paint!!.style = Paint.Style.STROKE
+        paint!!.strokeWidth = 5f
+        paint!!.color = Color.BLUE
+        canvas.drawRect(250F,250F,400F,400F, paint!!)
+
+        paintText!!.textSize = 20F
+        paintText!!.style = Paint.Style.FILL
+        paintText!!.color = Color.BLUE
+        canvas.drawText("Hello", 150F,200F, paintText!!)
+
+        canvas.drawARGB( 24,2,4,55)
+        var ctx1 = Path()
+        ctx1.moveTo(125F, 125F);
+        ctx1.lineTo(125F, 45F);
+        ctx1.lineTo(45F, 125F);
+        canvas.drawPath(ctx1,paint8!!)
+
+        paintTriangle!!.isAntiAlias = true
+        paintTriangle!!.style = Paint.Style.STROKE
+        paintTriangle!!.strokeWidth = 5f
+        paintTriangle!!.color = Color.BLUE
+        var ctx = Path()
+        ctx.moveTo(400F, 250F);
+        ctx.lineTo(300F, 400F);
+        ctx.lineTo(300F, 250F);
+        canvas.drawPath(ctx,paintTriangle!!)
+    }
+
+    /**
+     * This method acts as an event listener when a touch
+     * event is detected on the device.
+     */
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val touchX = event.x // Touch event of X coordinate
+        val touchY = event.y // touch event of Y coordinate
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+
+            }
+
+            MotionEvent.ACTION_UP -> {
+
+            }
+            else -> return false
+        }
+
+        //invalidate()
+        postInvalidate()
+        return true
+    }
+
+}
--- app/src/main/java/com/canvas/drawing/MainActivity.kt
+++ app/src/main/java/com/canvas/drawing/MainActivity.kt
@@ -1,21 +1,69 @@
 package com.canvas.drawing
 
+import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
 import com.canvas.drawing.databinding.ActivityMainBinding
-import java.lang.String
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
     var binding: ActivityMainBinding? = null
     var lastView : Int = 0
 
+    val requestPermission: ActivityResultLauncher<Array<kotlin.String>> = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+    { permissions ->
+        permissions.entries.forEach {
+            val perMissionName = it.key
+            val isGranted = it.value
+            //if permission is granted show a toast and perform operation
+            if (isGranted ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Permission granted now you can read the storage files.",
+                    Toast.LENGTH_LONG
+                ).show()
+                val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                lifecycleScope.launch{
+                    //reference the frame layout
+                   // val flDrawingView:FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                    //Save the image to the device
+                    saveBitmapFile(binding?.linearLayout?.let { it1 -> getBitmapFromView(it1) })
+                }
+            } else {
+                //Displaying another toast if permission is not granted and this time focus on
+                //    Read external storage
+                if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Oops you just denied the permission.",
+                        Toast.LENGTH_LONG
+                    ).show()
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +76,10 @@ class MainActivity : AppCompatActivity() {
         binding?.color?.setOnClickListener{
             selectColor()
         }
+        binding?.save?.setOnClickListener{
+           requestStoragePermission()
+        }
+
     }
 
     private fun selectColor() {
@@ -117,9 +169,144 @@ class MainActivity : AppCompatActivity() {
 
         dialog.show()
 
+    }
 
 
-
+    //create a method to requestStorage permission
+    private fun requestStoragePermission(){
+        // Check if the permission was denied and show rationale
+        if (
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            //call the rationale dialog to tell the user why they need to allow permission request
+            showRationaleDialog("Kids Drawing App","Kids Drawing App needs to Access Your External Storage")
+        }
+        else {
+            // You can directly ask for the permission.
+            //if it has not been denied then request for permission
+            //  The registered ActivityResultCallback gets the result of this request.
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
 
     }
+    /**  create rationale dialog
+     * Shows rationale dialog for displaying why the app needs permission
+     * Only shown if the user has denied the permission request previously
+     */
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+    // TODO(Step 4 : Getting and bitmap Exporting the image to your phone storage.)
+    /**
+     * Create bitmap from view and returns it
+     */
+    private fun getBitmapFromView(view: View): Bitmap {
+
+        //Define a bitmap with the same size as the view.
+        // CreateBitmap : Returns a mutable bitmap with the specified width and height
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
+        val canvas = Canvas(returnedBitmap)
+        //Get the view's background
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas)
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE)
+        }
+        // draw the view on the canvas
+        view.draw(canvas)
+        //return the bitmap
+        return returnedBitmap
+    }
+
+
+    // TODO(Step 2 : A method to save the image.)
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?):String{
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if (mBitmap != null) {
+
+                try {
+                    val bytes = ByteArrayOutputStream() // Creates a new byte array output stream.
+                    // The buffer capacity is initially 32 bytes, though its size increases if necessary.
+
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    /**
+                     * Write a compressed version of the bitmap to the specified outputstream.
+                     * If this returns true, the bitmap can be reconstructed by passing a
+                     * corresponding inputstream to BitmapFactory.decodeStream(). Note: not
+                     * all Formats support all bitmap configs directly, so it is possible that
+                     * the returned bitmap from BitmapFactory could be in a different bitdepth,
+                     * and/or may have lost per-pixel alpha (e.g. JPEG only supports opaque
+                     * pixels).
+                     *
+                     * @param format   The format of the compressed image
+                     * @param quality  Hint to the compressor, 0-100. 0 meaning compress for
+                     *                 small size, 100 meaning compress for max quality. Some
+                     *                 formats, like PNG which is lossless, will ignore the
+                     *                 quality setting
+                     * @param stream   The outputstream to write the compressed data.
+                     * @return true if successfully compressed to the specified stream.
+                     */
+
+                    val f = File(
+                        externalCacheDir?.absoluteFile.toString()
+                                + File.separator + "KidDrawingApp_" + System.currentTimeMillis() / 1000 + ".jpg"
+                    )
+                    // Here the Environment : Provides access to environment variables.
+                    // getExternalStorageDirectory : returns the primary shared/external storage directory.
+                    // absoluteFile : Returns the absolute form of this abstract pathname.
+                    // File.separator : The system-dependent default name-separator character. This string contains a single character.
+
+                    val fo =
+                        FileOutputStream(f) // Creates a file output stream to write to the file represented by the specified object.
+                    fo.write(bytes.toByteArray()) // Writes bytes from the specified byte array to this file output stream.
+                    fo.close() // Closes this file output stream and releases any system resources associated with this stream. This file output stream may no longer be used for writing bytes.
+                    result = f.absolutePath // The file absolute path is return as a result.
+                    //We switch from io to ui thread to show a toast
+                    runOnUiThread {
+                        if (!result.isEmpty()) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File saved successfully :$result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong while saving the file.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
+    }
+
+
 }
